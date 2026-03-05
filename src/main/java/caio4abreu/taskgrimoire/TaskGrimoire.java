40ac63a96659e206b06f9844d18fec174fd15f48
Caio4breu <abreuocaio@gmail.com>
2026-03-05 18:04:49 -0300

    Inicio do Projeto

---
+++ src/main/java/caio4abreu/taskgrimoire/TaskGrimoire.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package caio4abreu.taskgrimoire;
+
+/**
+ *
+ * @author Caio 4breu
+ */
+public class TaskGrimoire {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/caio4abreu/taskgrimoire/controller/AtividadeController.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package caio4abreu.taskgrimoire.controller;
+
+/**
+ *
+ * @author Caio 4breu
+ */
+public class AtividadeController {
+    
+}

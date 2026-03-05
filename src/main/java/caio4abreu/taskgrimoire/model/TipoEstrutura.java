27153e29966c85f79c4a651e75bca463c6c52d43
Caio Abreu <abreuocaio@gmail.com>
2026-03-05 20:11:32 -0300

    feat(model): adiciona enum TipoEstrutura com valores FILA e PILHA

--- src/main/java/caio4abreu/taskgrimoire/model/TipoEstrutura.java
+++ src/main/java/caio4abreu/taskgrimoire/model/TipoEstrutura.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
- */
 package caio4abreu.taskgrimoire.model;
 
 /**
@@ -9,5 +5,6 @@ package caio4abreu.taskgrimoire.model;
  * @author Caio 4breu
  */
 public enum TipoEstrutura {
-    
+    FILA,
+    PILHA
 }

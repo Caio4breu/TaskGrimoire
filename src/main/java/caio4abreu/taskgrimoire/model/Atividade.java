549388afc089d319098e08487eaa5adcfa407e99
Paulo <kct@gmail.com>
2026-03-05 19:45:40 -0300

    feat(model): adiciona classe Atividade com atributos, construtor e toString

--- src/main/java/caio4abreu/taskgrimoire/model/Atividade.java
+++ src/main/java/caio4abreu/taskgrimoire/model/Atividade.java
@@ -1,13 +1,41 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package caio4abreu.taskgrimoire.model;
+
+import java.time.LocalDateTime;
 
 /**
  *
  * @author Caio 4breu
  */
 public class Atividade {
+    private String titulo;
+    private String descricao;
+    private LocalDateTime dataRegistro;
+    private LocalDateTime dataTermino;
     
+    public Atividade(String titulo, String descricao){
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.dataRegistro = LocalDateTime.now();
+        this.dataTermino = null;
+    }
+    
+    // Getters & Setters
+    public String getTitulo(){ return titulo; }
+    public void setTitulo(String titulo){ this.titulo = titulo;}
+    
+    public String getDescricao() { return descricao; }
+    public void setDescricao(String descricao) { this.descricao = descricao; }
+    
+    public LocalDateTime getDataRegistro() { return dataRegistro; }
+    public void setDataRegistro (LocalDateTime dataRegistro) { this.dataRegistro = dataRegistro;}
+    
+    public LocalDateTime getDataTermino() { return dataTermino; }
+    public void setDataTermino(LocalDateTime dataTermino) { this.dataTermino = dataTermino; }
+    
+    @Override
+    public String toString(){
+        return "Atividade: " + titulo + "\n" +
+                "Descrição: " + descricao + "\n" +
+                "Data de inicio: " + dataRegistro;
+    }
 }

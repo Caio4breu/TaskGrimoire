44baf8949fbc701e5b488bfa5da5a7ca80b35ed0
Caio4breu <abreuocaio@gmail.com>
2026-03-06 11:18:45 -0300

    feat(repositories): adiciona interface ListaRepository

--- src/main/java/caio4abreu/taskgrimoire/repositories/ListaRepository.java
+++ src/main/java/caio4abreu/taskgrimoire/repositories/ListaRepository.java
@@ -1,9 +1,15 @@
 package caio4abreu.taskgrimoire.repositories;
 
+import caio4abreu.taskgrimoire.model.ListaDeAtividades;
+import java.util.List;
+
 /**
  *
  * @author Caio 4breu
  */
 public interface ListaRepository {
-    
+    public void salvar(ListaDeAtividades lista);
+    public ListaDeAtividades buscarPorNome(String nome);
+    public List<ListaDeAtividades> buscarTodos();
+    public void deletar(String nome);
 }

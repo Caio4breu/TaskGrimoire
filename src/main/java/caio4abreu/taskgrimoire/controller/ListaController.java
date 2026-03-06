726f17496a80632fec3fedc06a215c4a16c20e39
Caio4breu <abreuocaio@gmail.com>
2026-03-06 17:20:45 -0300

    feat(controller): adiciona ListaController

--- src/main/java/caio4abreu/taskgrimoire/controller/ListaController.java
+++ src/main/java/caio4abreu/taskgrimoire/controller/ListaController.java
@@ -1,13 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package caio4abreu.taskgrimoire.controller;
+
+import caio4abreu.taskgrimoire.model.ListaDeAtividades;
+import caio4abreu.taskgrimoire.model.TipoEstrutura;
+import caio4abreu.taskgrimoire.service.ListaService;
+import java.util.List;
 
 /**
  *
  * @author Caio 4breu
  */
 public class ListaController {
+    private ListaService lista = new ListaService();
     
+    public String criarLista(String nome, TipoEstrutura tipo){
+        try {
+            lista.criarLista(nome, tipo);
+            return "Lista criada com sucesso!";
+        } catch(IllegalArgumentException e) {
+            return e.getMessage();
+        }
+    }
+    
+    public ListaDeAtividades buscarLista(String nome){
+        return lista.buscarLista(nome);
+    }
+    
+    public List<ListaDeAtividades> listarTodas(){
+        return lista.listarTodas();
+    }
+    
+    public String deletarLista(String nome){
+        try{
+            lista.deletarLista(nome);
+            return "Lista deletada com sucesso!";
+        } catch (IllegalArgumentException e){
+            return e.getMessage();
+        }
+    }   
 }
